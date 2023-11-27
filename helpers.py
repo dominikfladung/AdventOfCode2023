8d77e2fd35c678814aea7f0afff3b36a63067423
Dominik Fladung <dominik.fladung@gmx.de>
2023-11-27 18:48:43 +0100

    add template files

---
+++ helpers.py
@@ -0,0 +1,10 @@
+"""Utility functions for the project."""
+
+def binary_to_decimal(binary: str) -> int:
+    """Converts a binary number to a decimal number."""
+    return int(binary, 2)
+
+def import_input(file_name: str) -> str:
+    """Reads a file and returns its content."""
+    with open(f"./inputs/{file_name}.txt", encoding="utf-8") as f:
+        return f.read().splitlines();
